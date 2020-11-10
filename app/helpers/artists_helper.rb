4430a16c85b5fc45aa79ab3c5320df49d344089c
zach6585 <zmines@wesleyan.edu>
2020-11-10 17:55:08 -0500

    commit tree 3b869407fa51b98882c6e2aef48b12788d9d9b62

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,12 @@
+require 'pry'
 module ArtistsHelper
+    def display_artist(song)
+        # binding.pry
+        if song.artist_id 
+            link_to "#{Artist.find(song.artist_id).name}" , artist_path(Artist.find(song.artist_id))
+ 
+        else 
+            link_to "Add Artist", edit_song_path(song)  
+        end 
+    end 
 end
